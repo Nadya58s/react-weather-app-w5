6978606fb30301ec3f02bb3f1d624ebffbdfb2b6
Nadia Sylvester <126354130+Nadya58s@users.noreply.github.com>
2023-07-01 15:44:51 +0100

    Added the GitHub url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project is created by Nadia Sylvester and is {""}
+        <a
+          href="https://github.com/Nadya58s/react-weather-app-w5"
+          target="_blank"
+        >
+          open-source on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-
