91371bf993f7076d53e55386a747ce47606741bd
Nadia Sylvester <126354130+Nadya58s@users.noreply.github.com>
2023-07-01 15:59:18 +0100

    Added bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project is created by Nadia Sylvester and is {""}
-        <a
-          href="https://github.com/Nadya58s/react-weather-app-w5"
-          target="_blank"
-        >
-          open-source on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+
+        <footer>
+          This project is created by Nadia Sylvester and is {""}
+          <a
+            href="https://github.com/Nadya58s/react-weather-app-w5"
+            target="_blank"
+          >
+            open-source on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
