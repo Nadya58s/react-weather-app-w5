ef97b1b626e16ab6a581307e3e41c5ef796ddbf6
Nadia Sylvester <126354130+Nadya58s@users.noreply.github.com>
2023-07-02 15:17:30 +0100

    -

--- src/App.js
+++ src/App.js
@@ -11,30 +11,16 @@ export default function App() {
         <Weather defaultCity="New York" />
 
         <footer>
-          This project was coded by{" "}
-          <a
-            href="https://www.delac.io/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Matt Delac
-          </a>{" "}
+          This project was coded by Nadia Sylvester
           and is{" "}
           <a
-            href="https://github.com/wecodeschool/react-weather-app"
+            href="https://github.com/Nadya58s/react-weather-app-w5"
             target="_blank"
             rel="noopener noreferrer"
           >
             open-sourced on GitHub
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://suspicious-beaver-111c4d.netlify.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hosted on Netlify
           </a>
+          
         </footer>
       </div>
     </div>
